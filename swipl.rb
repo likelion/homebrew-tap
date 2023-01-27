115c020ba42d0cb245a6062d94e7898fcfe0f138
Leonid Mokrushin <leonid.mokrushin@ericsson.com>
2023-01-27 20:26:56 +0100

    Bumped to version 9.1.4

--- swipl.rb
+++ swipl.rb
@@ -1,8 +1,8 @@
 class Swipl < Formula
   desc "ISO/Edinburgh-style Prolog interpreter (devel version)"
   homepage "http://www.swi-prolog.org/"
-  url "http://www.swi-prolog.org/download/devel/src/swipl-9.1.3.tar.gz"
-  sha256 "8b3355a82e0bd68efbc5d2feb49f5f0a1d953061396334f2a5e44e008fadc3d3"
+  url "http://www.swi-prolog.org/download/devel/src/swipl-9.1.4.tar.gz"
+  sha256 "ce064399da14611cf044fa22c32a0a5ec1df0ec5ffe0e21ae70518c39364f4f8"
   license "BSD-2-Clause"
   head "https://github.com/SWI-Prolog/swipl-devel.git"
 
